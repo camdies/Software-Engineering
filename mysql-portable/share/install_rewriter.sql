f2e51c995e8fbf1d04240a73c49ee179a84348a3
camdies <3151356198@qq.com>
2026-06-18 14:37:35 +0800

    fix MYSQL

---
+++ mysql-portable/share/install_rewriter.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c9faaa48b12878bb85e8bbb791dc6d008ab065f0e0eb15bb643d0759d7608b28
+size 2323
---
+++ mysql-portable/share/uninstall_rewriter.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b7ebda2402162219bf0a5954c35ff39f9d0442918d7496b35ff50addaecc8481
+size 1325
